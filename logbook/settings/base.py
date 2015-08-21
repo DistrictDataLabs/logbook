5795b5ac661277f532339f14cc18be9c2db3ded6
Benjamin Bengfort <benjamin@bengfort.com>
2015-08-21 13:59:17 -0500

    changed logo in login

--- logbook/settings/base.py
+++ logbook/settings/base.py
@@ -119,6 +119,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'social.apps.django_app.middleware.SocialAuthExceptionMiddleware',
 )
 
 ## Internationalization
