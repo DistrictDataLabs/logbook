63baf66c04fafad912de0cd5f341a06db9257de7
Benjamin Bengfort <benjamin@bengfort.com>
2015-08-21 13:51:54 -0500

    google authentication

--- logbook/urls.py
+++ logbook/urls.py
@@ -1,6 +1,26 @@
-from django.views.generic import TemplateView
+# logbook.urls
+# Application url definition and routers.
+#
+# Author:   Benjamin Bengfort <bbengfort@districtdatalabs.com>
+# Created:  Fri Aug 21 13:21:31 2015 -0500
+#
+# Copyright (C) 2015 District Data Labs
+# For license information, see LICENSE.txt
+#
+# ID: urls.py [] benjamin@bengfort.com $
+
+"""
+Application url definition and routers.
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
 from django.conf.urls import include, url
 from django.contrib import admin
+
+from logbook.views import HomePageView
 
 ##########################################################################
 ## URL Patterns
@@ -12,9 +32,9 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     # Application URLs
-    url(r'^$', TemplateView.as_view(template_name='site/home.html'), name='home'),
+    url(r'^$', HomePageView.as_view(), name='home'),
 
     # Authentication URLs
-    # url('', include('social.apps.django_app.urls', namespace='social')),
+    url('', include('social.apps.django_app.urls', namespace='social')),
     url('^accounts/', include('django.contrib.auth.urls')),
 ]
